6d0183851433c92df2e87ed238cba5b4852aa67f
Oscar King <Oscar.ken@gmail.com>
2023-08-02 13:47:46 +0800

    add namespace

--- Editor/Enum/NCatEnumCreateToolPage.cs
+++ Editor/Enum/NCatEnumCreateToolPage.cs
@@ -7,51 +7,54 @@ using UnityEditor;
 using UnityEngine;
 
 
-
-public class NCatEnumCreateToolPage : ScriptableSingleton<NCatEnumCreateToolPage>
+namespace NCat.Editor.Odin
 {
+    public class NCatEnumCreateToolPage : ScriptableSingleton<NCatEnumCreateToolPage>
+    {
 
 #if UNITY_EDITOR
-    [LabelText("资源路径")]
-    public string AssetRoot = "Asssets/Editor/";
-    
-    [LabelText("菜单显示名")]
-    public string MenuName = "枚举显示名";
+        [LabelText("资源路径")]
+        public string AssetRoot = "Assets/Editor/";
 
-    [LabelText("enum 类型名")]
-    public string GenEnumName;
+        [LabelText("菜单显示名")]
+        public string MenuName = "枚举显示名";
 
-        
-    [Button("创建新枚举", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
-    public void CreateNewEnum()
-    {
-        string file = $"{AssetRoot}Enum/{GenEnumName}.asset";
+        [LabelText("enum 类型名")]
+        public string GenEnumName;
 
-        if(File.Exists(file))
+
+        [Button("创建新枚举", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
+        public void CreateNewEnum()
         {
-            EditorUtility.DisplayDialog("Error", $"已经存在枚举：{file}", "OK");
-            return;
+            string file = $"{AssetRoot}Enum/{GenEnumName}.asset";
+
+            if (File.Exists(file))
+            {
+                EditorUtility.DisplayDialog("Error", $"已经存在枚举：{file}", "OK");
+                return;
+            }
+
+            //AssetDatabase.CreateFolder(AssetRoot, "Enums");
+
+            string dir = System.IO.Path.Combine(
+                Directory.GetCurrentDirectory(),
+                System.IO.Path.GetDirectoryName(file));
+            Debug.Log("Create Dir:" + dir);
+            Directory.CreateDirectory(dir);
+
+            string assetPath = file;
+
+            NCatEnumDefine newDef = ScriptableObject.CreateInstance<NCatEnumDefine>();
+            newDef.MenuName = MenuName;
+            newDef.GenEnumName = GenEnumName;
+
+            AssetDatabase.CreateAsset(newDef, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
-
-        //AssetDatabase.CreateFolder(AssetRoot, "Enums");
-
-        string dir = System.IO.Path.Combine(
-            Directory.GetCurrentDirectory(),
-            System.IO.Path.GetDirectoryName(file));
-        Debug.Log("Create Dir:" + dir);
-        Directory.CreateDirectory(dir);
-
-        string assetPath = file;
-
-        NCatEnumDefine newDef = ScriptableObject.CreateInstance<NCatEnumDefine>();
-        newDef.MenuName = MenuName;
-        newDef.GenEnumName = GenEnumName;
-
-        AssetDatabase.CreateAsset(newDef, assetPath);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-    }
 
 
 #endif
+    }
+
 }
--- Editor/Enum/NCatEnumDefine.cs
+++ Editor/Enum/NCatEnumDefine.cs
@@ -5,196 +5,200 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-
-[Serializable]
-public class NCatEnumDefineData
+namespace NCat.Editor.Odin
 {
-    [HideLabel]
-    [VerticalGroup("枚举名")]
-    public string Name;
 
-    [HideLabel]
-    [VerticalGroup("编辑器显示名")]
-    public string DisplayName;
-}
+    [Serializable]
+    public class NCatEnumDefineData
+    {
+        [HideLabel]
+        [VerticalGroup("枚举名")]
+        public string Name;
+
+        [HideLabel]
+        [VerticalGroup("编辑器显示名")]
+        public string DisplayName;
+    }
 
 
-[CreateAssetMenu(fileName = "EnumDefine", menuName = "NCatTools/创建枚举配置")]
-public class NCatEnumDefine : ScriptableObject
-{
-    [LabelText("枚举定义列表")]
-    [InfoBox("点击右边的 + 号新增")]
-    [ListDrawerSettings(ShowIndexLabels = false, ShowPaging = false, ShowItemCount = true, DraggableItems = true)]
-    [TableList(AlwaysExpanded = true)]
-    public List<NCatEnumDefineData> DefineListNew = new List<NCatEnumDefineData>();
+    [CreateAssetMenu(fileName = "EnumDefine", menuName = "NCatTools/创建枚举配置")]
+    public class NCatEnumDefine : ScriptableObject
+    {
+        [LabelText("枚举定义列表")]
+        [InfoBox("点击右边的 + 号新增")]
+        [ListDrawerSettings(ShowIndexLabels = false, ShowPaging = false, ShowItemCount = true, DraggableItems = true)]
+        [TableList(AlwaysExpanded = true)]
+        public List<NCatEnumDefineData> DefineListNew = new List<NCatEnumDefineData>();
 
 
 #if UNITY_EDITOR
-    [LabelText("菜单显示名")]
-    public string MenuName = "Define";
+        [LabelText("菜单显示名")]
+        public string MenuName = "Define";
 
-    [LabelText("enum 类型名")]
-    public string GenEnumName;
+        [LabelText("enum 类型名")]
+        public string GenEnumName;
 
-    [LabelText("enum 变量前辍")]
-    public string GenEnumPrefix;
+        [LabelText("enum 变量前辍")]
+        public string GenEnumPrefix;
 
-    [LabelText("是否为Mask型")]
-    public bool IsMask;
+        [LabelText("是否为Mask型")]
+        public bool IsMask;
 
-    [LabelText("自动生成None变量")]
-    public bool GenNoneItem = true;
+        [LabelText("自动生成None变量")]
+        public bool GenNoneItem = true;
 
-    [LabelText("自动生成Max变量")]
-    public bool GenMaxItem = true;
+        [LabelText("自动生成Max变量")]
+        public bool GenMaxItem = true;
 
-    [LabelText("开始变量数值")]
-    public int StartItemValue = 1;
+        [LabelText("开始变量数值")]
+        public int StartItemValue = 1;
 
-    [LabelText("代码生成目录")]
-    public string GenFolder = "/_AutoGenScripts/";
+        [LabelText("代码生成目录")]
+        public string GenFolder = "/_AutoGenScripts/";
 
 
 
-    // [TitleGroup("", Order = -100)]
-    [ButtonGroup(Order=-100)]
-    [Button("生成代码", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
-    public void GenCodeFile()
-    {
-        string str = Code;
-
-        string d = Application.dataPath + GenFolder;
-        string saveFile = d + GenEnumName + ".cs";
-        if (!System.IO.Directory.Exists(d))
+        // [TitleGroup("", Order = -100)]
+        [ButtonGroup(Order = -100)]
+        [Button("生成代码", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
+        public void GenCodeFile()
         {
-            System.IO.Directory.CreateDirectory(d);
-        }
-        System.IO.File.WriteAllText(saveFile, str);
+            string str = Code;
 
-        AssetDatabase.Refresh();
-    }
-
-    // [TitleGroup("", Order = -100)]
-    [ButtonGroup(Order=-100)]
-    [Button("保存", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
-    public void SaveAssets()
-    {
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-    }
-
-    // [TitleGroup("", Order = -100)]
-    [ButtonGroup(Order=-100)]
-    [Button("复制变量列表", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
-    public void CopyNameList()
-    {
-        string tmpStr = "";
-        for (int i = 0; i < DefineListNew.Count; i++)
-        {
-            NCatEnumDefineData data = DefineListNew[i];
-            if (!string.IsNullOrEmpty(data.Name))
+            string d = Application.dataPath + GenFolder;
+            string saveFile = d + GenEnumName + ".cs";
+            if (!System.IO.Directory.Exists(d))
             {
-                tmpStr += data.Name + "\r\n";
+                System.IO.Directory.CreateDirectory(d);
             }
+            System.IO.File.WriteAllText(saveFile, str);
+
+            AssetDatabase.Refresh();
         }
 
-        CopyToClipboard(tmpStr);
-    }
-
-    // [TitleGroup("", Order = -100)]
-    [ButtonGroup(Order=-100)]
-    [Button("复制描述列表", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
-    public void CopyDescList()
-    {
-        string tmpStr = "";
-        for (int i = 0; i < DefineListNew.Count; i++)
+        // [TitleGroup("", Order = -100)]
+        [ButtonGroup(Order = -100)]
+        [Button("保存", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
+        public void SaveAssets()
         {
-            NCatEnumDefineData data = DefineListNew[i];
-            if (!string.IsNullOrEmpty(data.Name))
-            {
-                tmpStr += data.DisplayName + "\r\n";
-            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
 
-        CopyToClipboard(tmpStr);
-    }
-
-    public static void CopyToClipboard(string str)
-    {
-        GUIUtility.systemCopyBuffer = str;
-    }
-
-
-    [InfoBox("@Code")]
-    [ReadOnly]
-    [ShowInInspector]
-    public string Code
-    {
-        get
+        // [TitleGroup("", Order = -100)]
+        [ButtonGroup(Order = -100)]
+        [Button("复制变量列表", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
+        public void CopyNameList()
         {
-            string str = "//\r\n";
-            str += "// Auto generated code, DON NOT modify by manual!!!\r\n";
-            str += "//\r\n";
-            str += "\r\n";
-            str += "\r\n";
-            str += "using Sirenix.OdinInspector;\r\n\r\n";
-
-            if (IsMask)
-            {
-                str += "[System.Flags]\r\n";
-            }
-
-            str += "public enum " + GenEnumName + "\r\n{\r\n";
-
-            if (GenNoneItem)
-            {
-                str += $"\t[LabelText(\"无\")]\r\n";
-                str += "\t" + GenEnumPrefix + "None = 0,\r\n\r\n";
-            }
-
-            int maxCnt = 0;
-
-            string AllMask = "";
-
+            string tmpStr = "";
             for (int i = 0; i < DefineListNew.Count; i++)
             {
                 NCatEnumDefineData data = DefineListNew[i];
                 if (!string.IsNullOrEmpty(data.Name))
                 {
-                    string dispName = string.IsNullOrEmpty(data.DisplayName) ? data.Name : data.DisplayName;
-                    str += $"\t[LabelText(\"{dispName}\")]\r\n";
-                    if (IsMask)
-                    {
-                        AllMask += $"{GenEnumPrefix}{data.Name}";
-                        if (i < DefineListNew.Count - 1)
-                        {
-                            AllMask += " | ";
-                        }
-
-                        str += $"\t{GenEnumPrefix}{data.Name} = 1 << {i + 1},\r\n\r\n";
-                    }
-                    else
-                    {
-                        str += $"\t{GenEnumPrefix}{data.Name} = {i + StartItemValue},\r\n\r\n";
-                    }
-
-                    ++maxCnt;
+                    tmpStr += data.Name + "\r\n";
                 }
             }
 
-            if (IsMask)
-            {
-                str += $"\t{GenEnumPrefix}All = {AllMask}\r\n";
-            }
-            else if (GenMaxItem)
-            {
-                str += "\t" + GenEnumPrefix + "Max = " + ++maxCnt + "\r\n";
-            }
-
-            str += "}";
-
-            return str;
+            CopyToClipboard(tmpStr);
         }
-    }
+
+        // [TitleGroup("", Order = -100)]
+        [ButtonGroup(Order = -100)]
+        [Button("复制描述列表", ButtonSizes.Large), GUIColor(0.3f, 0.8f, 1)]
+        public void CopyDescList()
+        {
+            string tmpStr = "";
+            for (int i = 0; i < DefineListNew.Count; i++)
+            {
+                NCatEnumDefineData data = DefineListNew[i];
+                if (!string.IsNullOrEmpty(data.Name))
+                {
+                    tmpStr += data.DisplayName + "\r\n";
+                }
+            }
+
+            CopyToClipboard(tmpStr);
+        }
+
+        public static void CopyToClipboard(string str)
+        {
+            GUIUtility.systemCopyBuffer = str;
+        }
+
+
+        [InfoBox("@Code")]
+        [ReadOnly]
+        [ShowInInspector]
+        public string Code
+        {
+            get
+            {
+                string str = "//\r\n";
+                str += "// Auto generated code, DON NOT modify by manual!!!\r\n";
+                str += "//\r\n";
+                str += "\r\n";
+                str += "\r\n";
+                str += "using Sirenix.OdinInspector;\r\n\r\n";
+
+                if (IsMask)
+                {
+                    str += "[System.Flags]\r\n";
+                }
+
+                str += "public enum " + GenEnumName + "\r\n{\r\n";
+
+                if (GenNoneItem)
+                {
+                    str += $"\t[LabelText(\"无\")]\r\n";
+                    str += "\t" + GenEnumPrefix + "None = 0,\r\n\r\n";
+                }
+
+                int maxCnt = 0;
+
+                string AllMask = "";
+
+                for (int i = 0; i < DefineListNew.Count; i++)
+                {
+                    NCatEnumDefineData data = DefineListNew[i];
+                    if (!string.IsNullOrEmpty(data.Name))
+                    {
+                        string dispName = string.IsNullOrEmpty(data.DisplayName) ? data.Name : data.DisplayName;
+                        str += $"\t[LabelText(\"{dispName}\")]\r\n";
+                        if (IsMask)
+                        {
+                            AllMask += $"{GenEnumPrefix}{data.Name}";
+                            if (i < DefineListNew.Count - 1)
+                            {
+                                AllMask += " | ";
+                            }
+
+                            str += $"\t{GenEnumPrefix}{data.Name} = 1 << {i + 1},\r\n\r\n";
+                        }
+                        else
+                        {
+                            str += $"\t{GenEnumPrefix}{data.Name} = {i + StartItemValue},\r\n\r\n";
+                        }
+
+                        ++maxCnt;
+                    }
+                }
+
+                if (IsMask)
+                {
+                    str += $"\t{GenEnumPrefix}All = {AllMask}\r\n";
+                }
+                else if (GenMaxItem)
+                {
+                    str += "\t" + GenEnumPrefix + "Max = " + ++maxCnt + "\r\n";
+                }
+
+                str += "}";
+
+                return str;
+            }
+        }
 #endif
+    }
+
 }
